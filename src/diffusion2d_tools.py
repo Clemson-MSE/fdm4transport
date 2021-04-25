0bc3eebbccf75583e9bca39069e8239b694305cd
Ulf Schiller <uschill@clemson.edu>
2021-04-24 22:24:41 -0400

    Update diffusion2d_tools.py scripts

--- src/diffusion2d_tools.py
+++ src/diffusion2d_tools.py
@@ -1,8 +1,15 @@
-def animate3d(C,frames=100,interval=50):
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm, animation
+
+def animate3d(C,dt,frames=100,interval=50,cmin=0,cmax=1):
   """Creates an animation of a time series of scalar values on a 2D grid.
      The scalar field is plotted as a surface mesh."""
   step = len(C)//frames
   C = np.vstack([C[0],C[1::step]])
+
+  n = int(np.sqrt(len(C[0])))
+  (x,y) = np.meshgrid(range(n),range(n))
 
   def init():
     surf = ax.plot_surface(x,y,C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
@@ -14,9 +21,6 @@ def animate3d(C,frames=100,interval=50):
     surf = ax.plot_surface(x,y,C[i].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
     time_text.set_text('t={0:.4f} s'.format(i*dt*step))
     return (surf,time_text)
-
-  cmax = 1
-  cmin = center_concentration(n,phi)
 
   fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
   surf = ax.plot_surface(x,y,C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
@@ -32,12 +36,14 @@ def animate3d(C,frames=100,interval=50):
   plt.close()
   return ani
 
-def animate2d(C,frames=100,interval=50):
+def animate2d(C,dt,cmin=1,cmax=1,frames=100,interval=50):
     """Creates an animation of a time series of scalar values on a 2D grid.
        The scalar field is plotted as a heatmap."""
     step = len(C)//frames
     C = np.vstack([C[0],C[1::step]])
 
+    n = int(np.sqrt(len(C[0])))
+    
     def init():
       surf = ax.imshow(C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
       time_text.set_text('t={0:.4f} s'.format(0))
@@ -47,9 +53,6 @@ def animate2d(C,frames=100,interval=50):
       surf = ax.imshow(C[i].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
       time_text.set_text('t={0:.4f} s'.format(i*dt*step))
       return (surf,time_text)
-
-    cmax = 1
-    cmin = center_concentration(n,phi)
 
     fig, ax = plt.subplots()
     surf = ax.imshow(C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
