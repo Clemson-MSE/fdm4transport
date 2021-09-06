5cfb21ffb5936c0f44def8490b66074e9765ad81
Ulf Schiller <uschill@clemson.edu>
2021-09-06 12:55:45 -0400

    Correct dimensionless time in 2D diffusion notebook

--- lib/diffusion2d_tools.py
+++ lib/diffusion2d_tools.py
@@ -13,18 +13,18 @@ def animate3d(C,dt,phi,frames=100,interval=50,cmin=0,cmax=1):
 
   def init():
     surf = ax.plot_surface(x,y,C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-    time_text.set_text('t={0:.4f} s'.format(0))
+    time_text.set_text('t={0:.4f}'.format(0))
     return (surf,time_text)
 
   def animate(i):
     #ax.collections.clear()
     surf = ax.plot_surface(x,y,C[i].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-    time_text.set_text('t={0:.4f} s'.format(i*dt*step))
+    time_text.set_text('t={0:.4f}'.format(i*dt*step))
     return (surf,time_text)
 
   fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
   surf = ax.plot_surface(x,y,C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-  time_text = ax.text2D(0.7, 0.9, 't=0 s', transform=ax.transAxes, fontsize=12)
+  time_text = ax.text2D(0.75, 0.9, 't=0', transform=ax.transAxes, fontsize=12)
   phi_text = ax.text2D(0.1, 0.9, r'$\phi=$%.2f'%phi, transform=ax.transAxes, fontsize=12)
   fig.colorbar(surf, label=r'$C/C_0$')
   ax.axes.set_zlim3d(bottom=cmin, top=1)
@@ -47,17 +47,17 @@ def animate2d(C,dt,phi,cmin=1,cmax=1,frames=100,interval=50):
     
     def init():
       surf = ax.imshow(C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-      time_text.set_text('t={0:.4f} s'.format(0))
+      time_text.set_text('t={0:.4f}'.format(0))
       return (surf,time_text)
 
     def animate(i):
       surf = ax.imshow(C[i].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-      time_text.set_text('t={0:.4f} s'.format(i*dt*step))
+      time_text.set_text('t={0:.4f}'.format(i*dt*step))
       return (surf,time_text)
 
     fig, ax = plt.subplots()
     surf = ax.imshow(C[0].reshape(n,n),cmap=cm.coolwarm,vmin=cmin,vmax=cmax)
-    time_text = ax.text(0.6, 0.9, 't=0 s', transform=ax.transAxes, color='w', fontsize=14)
+    time_text = ax.text(0.65, 0.9, 't=0', transform=ax.transAxes, color='w', fontsize=14)
     phi_text = ax.text(0.05, 0.9, r'$\phi=$%.2f'%phi, transform=ax.transAxes, color='w', fontsize=14)
     fig.colorbar(surf, label=r'$C/C_0$')
     ax.set_title('Concentration Profile')
